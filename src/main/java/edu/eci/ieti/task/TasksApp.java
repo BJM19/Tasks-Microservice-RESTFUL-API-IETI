44de5ba370a5ce0becaee010132f685f71956bcd
BJM19 <57643084+BJM19@users.noreply.github.com>
2022-02-18 19:11:31 -0500

    Código Base y Controller

---
+++ src/main/java/edu/eci/ieti/task/TasksApp.java
@@ -0,0 +1,13 @@
+package edu.eci.ieti.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasksApp.class, args);
+	}
+
+}
---
+++ src/test/java/edu/eci/ieti/task/TasksAppTests.java
@@ -0,0 +1,13 @@
+package edu.eci.ieti.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
